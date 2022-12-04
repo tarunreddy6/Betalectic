2c234de1c1373f993be8536be41c06bbb3f5b3b6
TarunReddyKalluri <55652043+tarunreddy6@users.noreply.github.com>
2022-12-04 18:02:37 +0530

    Add files via upload

---
+++ src/app/component/addfavorite/addfavorite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addfavorite',
+  templateUrl: './addfavorite.component.html',
+  styleUrls: ['./addfavorite.component.css']
+})
+export class AddfavoriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
